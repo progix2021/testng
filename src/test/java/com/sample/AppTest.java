1ffcf9eec368f9c4e72f28991dc5a3e37ed7c459
SomeOne <some@one.com>
2021-03-13 11:49:46 +0530

    Init

---
+++ src/test/java/com/sample/AppTest.java
@@ -0,0 +1,39 @@
+package com.sample;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    @Test
+    public void shouldAnswerWithTrue() throws InterruptedException, IOException {
+        System.out.println("Sample");
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.google.co.in");
+        WebElement element = driver.findElement(By.name("q"));
+        element.sendKeys("Cats");
+        Thread.sleep(1000);
+        TakesScreenshot screenshot = (TakesScreenshot) driver;
+        File file = screenshot.getScreenshotAs(OutputType.FILE);
+        File destFile = new File("screenShot.png");
+        FileUtils.copyFile(file, destFile);
+        driver.quit();
+        Assert.assertTrue(true);
+    }
+}
